89d5b114fa30d70964aca78c0aa267ab6b1eb969
jinwanwuye <163765548+jinwanwuye@users.noreply.github.com>
2025-03-08 18:00:37 +0700

    更新 install-immortalwrt-wan.sh

--- install-immortalwrt-wan.sh
+++ install-immortalwrt-wan.sh
@@ -1,3 +1,8 @@
+#!/bin/bash
+
+#进入root模式
+sudo i
+
 #下载ImmortalWrt wan口固件
 wget -O imm.img.gz "https://raw.githubusercontent.com/jinwanwuye/openwrt/refs/heads/main/immortalwrt-24.10.0-x86-64-generic-ext4-combined-efi-wan.img.gz"
 
@@ -5,7 +10,7 @@ wget -O imm.img.gz "https://raw.githubusercontent.com/jinwanwuye/openwrt/refs/he
 gzip -d imm.img.gz
 
 #获取安装的盘符sda或者vda等等
-AAA=lsblk -l|grep -i boot -B 10 |grep -i disk |awk '{print $1}'
+AAA="lsblk -l|grep -i boot -B 10 |grep -i disk |awk '{print $1}'"
 
 #将固件dd到当前vps上的/dev/磁盘上
 dd if=imm.img of=/dev/${AAA} bs=4M status=progress oflag=sync
