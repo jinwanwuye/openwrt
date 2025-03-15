1cc72656eb39511e899ce70fa02ed09590e2a9ed
jinwanwuye <163765548+jinwanwuye@users.noreply.github.com>
2025-03-15 11:12:35 +0700

    1

--- install-gocloud.sh
+++ install-gocloud.sh
@@ -10,7 +10,7 @@ wget -O imm.img.gz "https://raw.githubusercontent.com/jinwanwuye/openwrt/refs/he
 gzip -d imm.img.gz
 
 #获取安装的盘符sda或者vda等等
-AAA=$(lsblk -l|grep -i boot -B 1 |grep -i disk |awk '{print $1}')
+AAA=$(lsblk -l|grep -i boot -B 5 |grep -i disk |awk '{print $1}')
 
 #将固件dd到当前vps上的/dev/磁盘上
 sudo dd if=imm.img of=/dev/${AAA} bs=4M status=progress oflag=sync
